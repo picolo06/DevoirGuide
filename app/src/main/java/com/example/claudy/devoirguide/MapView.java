bd1bb9eac60b5864089c5af738f2aa6ce120d239
picolo06 <picolo06@yahoo.fr>
2017-06-22 10:28:59 -0500

    o.o.o

--- app/src/main/java/com/example/claudy/devoirguide/MapView.java
+++ app/src/main/java/com/example/claudy/devoirguide/MapView.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +26,7 @@ public class MapView extends AppCompatActivity implements OnMapReadyCallback /*,
     float latitude;
     float longitude;
     String name;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +46,9 @@ public class MapView extends AppCompatActivity implements OnMapReadyCallback /*,
         latitude = intent.getExtras().getFloat("lag");
                 longitude = intent.getExtras().getFloat("longi");
         name = intent.getExtras().getString("namee");
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingActionButton.bringToFront();
         /*
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling

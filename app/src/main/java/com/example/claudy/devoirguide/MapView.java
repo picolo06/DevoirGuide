94ec68b3a9abb32b6800388096b5a5e08e879195
picolo06 <picolo06@yahoo.fr>
2017-06-22 15:22:00 -0500

    o

--- app/src/main/java/com/example/claudy/devoirguide/MapView.java
+++ app/src/main/java/com/example/claudy/devoirguide/MapView.java
@@ -10,6 +10,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,7 +23,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapView extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+public class MapView extends AppCompatActivity implements OnMapReadyCallback /*, LocationListener */{
 
     GoogleMap map;
     LocationManager locationManager;
@@ -35,6 +37,10 @@ public class MapView extends AppCompatActivity implements OnMapReadyCallback, Lo
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view);
 
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
 
@@ -53,7 +59,7 @@ public class MapView extends AppCompatActivity implements OnMapReadyCallback, Lo
         floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         //floatingActionButton.bringToFront();
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      /*  if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -62,8 +68,8 @@ public class MapView extends AppCompatActivity implements OnMapReadyCallback, Lo
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, this);
+        }*/
+       // locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, this);
 
     }
 
@@ -72,26 +78,20 @@ public class MapView extends AppCompatActivity implements OnMapReadyCallback, Lo
         map = googleMap;
 
         Toast.makeText(MapView.this, latitude + "         " + longitude, Toast.LENGTH_LONG).show();
-       
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        map.setMyLocationEnabled(true);
-        LatLng location = new LatLng( longitude ,latitude);
+
+
+        //map.setMyLocationEnabled(true);
+
+        //double Longigi =18.55541;
+       // double Latiti = Double.parseDouble("-72.30660");
+        LatLng location = new LatLng(   longitude, latitude);
         map.addMarker(new MarkerOptions().position(location).title(name));
         map.moveCamera(CameraUpdateFactory.newLatLng(location));
 
 
     }
 
-    @Override
+  /*  @Override
     public void onLocationChanged(Location location) {
 
         map.clear();
@@ -136,5 +136,20 @@ public class MapView extends AppCompatActivity implements OnMapReadyCallback, Lo
         MapView.this.startActivity(intent);
     }
 
+*/
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
